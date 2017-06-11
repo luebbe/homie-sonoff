0e0ac7d8ce0d6dc6feb94ee3bc2051b930e1e298
Lübbe <luebbe.opensource@gmail.com>
2017-06-11 17:22:10 +0200

    Initial firmware for a Sonoff wifi relay

---
+++ src/homie-sonoff.cpp
@@ -0,0 +1,40 @@
+#define FW_NAME "sonoff"
+#define FW_VERSION "1.0.2"
+
+#include <Homie.h>
+#include "homie-node-collection.h"
+
+// I prefer to see the first ESP Boot messages as well, hence 74880 instead of 115200
+#define SERIAL_SPEED 74880
+
+const int PIN_BUTTON = 0;
+const int PIN_RELAY = 12;
+const int PIN_LED = 13;
+
+RelayNode relayNode("relay", PIN_RELAY, PIN_LED);
+
+// Initialize button node with callback to button press
+ButtonNode buttonNode("button", PIN_BUTTON, []() {
+  relayNode.toggleRelay();
+});
+
+void setup() {
+  Serial.begin(SERIAL_SPEED);
+  Serial << endl << endl;
+
+  welcome();
+  otaSetup();
+
+  pinMode(PIN_BUTTON, INPUT_PULLUP);
+
+  Homie_setFirmware(FW_NAME, FW_VERSION);
+
+  Homie.disableResetTrigger();
+  Homie.disableLedFeedback();
+  Homie.setup();
+}
+
+void loop() {
+  Homie.loop();
+  otaLoop();
+}
