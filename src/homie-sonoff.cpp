447d2bfbe3e972788acadc5b5752b37dd1f1e24b
Lübbe <luebbe.opensource@gmail.com>
2018-11-22 21:07:40 +0100

    Make use of current homie-node-collection Add DHT22

--- src/homie-sonoff.cpp
+++ src/homie-sonoff.cpp
@@ -1,8 +1,10 @@
 #define FW_NAME "sonoff"
-#define FW_VERSION "1.0.3"
+#define FW_VERSION "1.0.4"
 
 #include <Homie.h>
-#include "homie-node-collection.h"
+#include "ota.hpp"
+#include "welcome.hpp"
+#include "homie-node-collection.hpp"
 
 // I prefer to see the first ESP Boot messages as well, hence 74880 instead of 115200
 #define SERIAL_SPEED 74880
@@ -10,11 +12,14 @@
 const int PIN_BUTTON = 0;
 const int PIN_RELAY = 12;
 const int PIN_LED = 13;
+const int PIN_DHT = 14;
 
 // Setup OTA logging via Homie logger
 OtaLogger ota;
+Welcome welcome(FW_NAME, FW_VERSION);
 
 RelayNode relayNode("relay", PIN_RELAY, PIN_LED);
+DHT22Node dht22Node("dht22", PIN_DHT);
 
 // Initialize button node with callback to button press
 ButtonNode buttonNode("button", PIN_BUTTON, []() {
@@ -34,7 +39,8 @@ void setup() {
   Serial.begin(SERIAL_SPEED);
   Serial << endl << endl;
 
-  welcome();
+  welcome.show();
+  ota.setup();
 
   pinMode(PIN_BUTTON, INPUT_PULLUP);
 
