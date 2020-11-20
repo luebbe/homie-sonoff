252b5eb2bedb22aadb67aeb02daa2a9ad2fff7e1
Lübbe Onken <luebbe.opensource@gmail.com>
2020-11-20 09:39:29 +0100

    remove homie-ota, switch to own version of homie-esp8266

--- src/homie-sonoff.cpp
+++ src/homie-sonoff.cpp
@@ -1,9 +1,7 @@
 #define FW_NAME "sonoff-dht22"
-#define FW_VERSION "1.0.6"
+#define FW_VERSION "1.0.7"
 
 #include <Homie.hpp>
-#include "ota.hpp"
-#include "welcome.hpp"
 #include "ButtonNode.hpp"
 #include "DHT22Node.hpp"
 #include "RelayNode.hpp"
@@ -15,28 +13,13 @@ const int PIN_RELAY = 12;
 const int PIN_LED = 13;
 const int PIN_DHT = 14;
 
-// Setup OTA logging via Homie logger
-OtaLogger ota;
-Welcome welcome(FW_NAME, FW_VERSION);
-
-RelayNode relayNode("relay", PIN_RELAY, PIN_LED);
-DHT22Node dht22Node("dht22", PIN_DHT);
+RelayNode relayNode("relay", "relay", PIN_RELAY, PIN_LED);
+DHT22Node dht22Node("dht22", "dht22", PIN_DHT);
 
 // Initialize button node with callback to button press
-ButtonNode buttonNode("button", PIN_BUTTON, []() {
+ButtonNode buttonNode("button", "button", PIN_BUTTON, []() {
   relayNode.toggleRelay();
 });
-
-void setupHandler()
-{
-  // This is called after the MQTT_CONNECTED event
-  ota.setup();
-}
-
-void loopHandler()
-{
-  ota.loop();
-}
 
 void setup()
 {
@@ -44,19 +27,11 @@ void setup()
   Serial << endl
          << endl;
 
-  welcome.show();
-  ota.setup();
-
   pinMode(PIN_BUTTON, INPUT_PULLUP);
 
   Homie_setFirmware(FW_NAME, FW_VERSION);
 
   Homie.disableResetTrigger();
-  Homie.disableLedFeedback();
-
-  Homie.setSetupFunction(setupHandler);
-  Homie.setLoopFunction(loopHandler);
-
   Homie.setup();
 }
 
